31d791ac58cd95d8d8a57f9876b7452e1b75c788
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-03 00:26:11 +0100

    added first commit

---
+++ MyBlazorWebapp/Data/Profile.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBlazorWebapp.Data
+{
+	public class Profile
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+	}
+}
---
+++ MyBlazorWebapp/StateObjects/ProfileState.cs
@@ -0,0 +1,32 @@
+﻿namespace MyBlazorWebapp.StateObjects
+{
+	public class ProfileState
+	{
+		public string? FirstName
+		{
+			get => _currentFirstName;
+			set
+			{
+				_currentFirstName = value;
+				StateHasChanged();
+			}
+		}
+
+		public string? LastName
+		{
+			get => _currentLastName;
+			set
+			{
+				_currentLastName = value;
+				StateHasChanged();
+			}
+		}
+
+		private string? _currentFirstName;
+		private string? _currentLastName;
+
+		public event EventHandler StateChangedHandler;
+
+		private void StateHasChanged() => StateChangedHandler.Invoke(this, EventArgs.Empty);
+	}
+}
